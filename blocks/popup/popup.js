b83e8b3fb89185fec86d59c570d23e953672b5e6
Arbuznik <arbyzov.nikita@yandex.ru>
2021-09-23 14:04:21 +0300

    make bugfixes after review

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,26 +1,29 @@
+let form = document.querySelector('.form');
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button'); 
-let saveButton = document.querySelector('.form__save-button');
 let userName = document.querySelector('.profile__title');
 let userJob = document.querySelector('.profile__subtitle');
 let nameInput = document.querySelector('.form__input_type_name');
 let jobInput = document.querySelector('.form__input_type_job');
 
-nameInput.value = userName.textContent;
-jobInput.value = userJob.textContent;
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+form.addEventListener('submit', handleProfileFormSubmit);
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-saveButton.addEventListener('submit', formSubmitHandler);
-
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+function openPopup() {
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
+  popup.classList.add('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userJob.textContent = jobInput.value;
-  togglePopup();
+  closePopup();
 }
