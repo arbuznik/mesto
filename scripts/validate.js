1a56540981e29e14252de71847c9ca781b69dcd1
Arbuznik <arbyzov.nikita@yandex.ru>
2021-10-21 18:40:38 +0300

    feat: add validate config

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,18 +1,29 @@
-function enableFormValidation(forms) {
+const pageConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__save-button',
+  inactiveButtonClass: 'form__save-button_disabled',
+  inputErrorClass: 'form__input_error',
+  errorClass: 'form__input-error_active'
+};
+
+function enableFormValidation(pageConfig) {
+  const forms = Array.from(document.querySelectorAll(pageConfig.formSelector));
+
   forms.forEach(form => {
     form.addEventListener('submit', (evt) => {
       evt.preventDefault();
     })
 
     setEventListeners(form);
-  })
+  })  
 }
 
-enableFormValidation(forms);
+enableFormValidation(pageConfig);
 
 function setEventListeners(form) {
-  const formInputs = Array.from(form.querySelectorAll('.form__input'));
-  const submitButton = form.querySelector('.form__save-button');
+  const formInputs = Array.from(form.querySelectorAll(pageConfig.inputSelector));
+  const submitButton = form.querySelector(pageConfig.submitButtonSelector);
 
   toggleSubmitButtonState(formInputs, submitButton);
 
@@ -34,23 +45,23 @@ function isInputValid(form, input) {
 
 function showInputError(form, input, errorMessage) {
   const inputError = form.querySelector(`.${input.id}-error`);
-  input.classList.add('form__input_error');
-  inputError.classList.add('form__input-error_active');
+  input.classList.add(pageConfig.inputErrorClass);
+  inputError.classList.add(pageConfig.errorClass);
   inputError.textContent = errorMessage;
 }
 
 function hideInputError(form, input) {
   const inputError = form.querySelector(`.${input.id}-error`);
-  input.classList.remove('form__input_error');
-  inputError.classList.remove('form__input-error_active');
+  input.classList.remove(pageConfig.inputErrorClass);
+  inputError.classList.remove(pageConfig.errorClass);
   inputError.textContent = '';
 }
 
 function toggleSubmitButtonState(inputs, submitButton) {
   if (areAllInputsValid(inputs)) {
-    submitButton.classList.remove('form__save-button_disabled');
+    submitButton.classList.remove(pageConfig.inactiveButtonClass);
   } else {
-    submitButton.classList.add('form__save-button_disabled');
+    submitButton.classList.add(pageConfig.inactiveButtonClass);
   }
 }
 
