beb60b7ed9a8be57f2772bdbde876689349959f3
Arbuznik <arbyzov.nikita@yandex.ru>
2021-11-27 16:48:06 +0300

    bugfix: proper listener delete

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,15 +2,16 @@ export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._closeButton = this._popup.querySelector('.popup__close-button');
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
     this._popup.classList.add('popup_opened');
   }
 
   close() {
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
     this._popup.classList.remove('popup_opened');
   }
 
